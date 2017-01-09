0a1017ace7a63936202744447d6ea3f4995d4e9f
Bernardo Vieira <bernardo5@live.com.pt>
2017-01-09 03:48:42 +0000

    NoDependencyTest

--- src/main/java/obernardovieira/dimusique/DiMusique.java
+++ src/main/java/obernardovieira/dimusique/DiMusique.java
@@ -1,12 +1,13 @@
 package obernardovieira.dimusique;
 
-import obernardovieira.dimusique.visual.View;
+//import obernardovieira.dimusique.visual.View;
 
 public class DiMusique 
 {
     public static void main(String[] args)
     {
-        View view = new View();
-        view.run();
+        //View view = new View();
+        //view.run();
+        System.out.println("HelloWorld!");
     }
 }
